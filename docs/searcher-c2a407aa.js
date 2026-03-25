94806541e0e686f9b7c8c5eb9e8656b477dd4983
Quentin Diebold (CI) <pro@quentindiebold.com>
2026-03-25 15:36:49 +0000

    chore(ci): publish release

--- docs/searcher-c2a407aa.js
+++ docs/searcher-c2a407aa.js
@@ -451,7 +451,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-02270dd6.js',
+                path_to_root + 'searchindex-44101b25.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
