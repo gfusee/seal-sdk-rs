376a72747f7175e8dd2bf86693fa1f75b4695d45
Quentin Diebold (CI) <pro@quentindiebold.com>
2026-06-30 13:49:19 +0000

    chore(ci): publish release

--- docs/searcher-09f2665d.js
+++ docs/searcher-09f2665d.js
@@ -156,7 +156,7 @@ window.search = window.search || {};
         const teaser = makeTeaser(escapeHTML(result.doc.body), searchterms);
         teaser_count++;
 
-        // The ?URL_MARK_PARAM= parameter belongs inbetween the page and the #heading-anchor
+        // The ?URL_MARK_PARAM= parameter belongs in between the page and the #heading-anchor
         const url = doc_urls[result.ref].split('#');
         if (url.length === 1) { // no anchor found
             url.push('');
@@ -355,6 +355,9 @@ window.search = window.search || {};
                         marker.unmark();
                     }, 300);
                 }
+                // also removes the `?URL_MARK_PARAM=` search param so that
+                // in-page navigation doesn't make highlights unexpectedly appear again
+                setSearchUrlParameters('', 'replace');
             };
 
             for (let i = 0; i < markers.length; i++) {
@@ -371,7 +374,7 @@ window.search = window.search || {};
             e.shiftKey ||
             e.target.type === 'textarea' ||
             e.target.type === 'text' ||
-            !hasFocus() && /^(?:input|select|textarea)$/i.test(e.target.nodeName)
+            !hasFocus() && mdbook_something_else_has_focus(e)
         ) {
             return;
         }
